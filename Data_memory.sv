95fb9db20cddaeabdad8cc43d09e22d2c4e8a6c7
Nassersh1250 <nassersh1200@gmail.com>
2024-12-24 15:09:24 +0300

    Add files via upload

--- Data_memory.sv
+++ Data_memory.sv
@@ -2,10 +2,27 @@
 
 module Data_memory(
     input logic clk,resetn,memwrite,
-    input logic [31:0]addr, wdata,
-    output logic [31:0] rdata
+    input logic [31:0]addr, 
+    input reg [31:0]wdata,
+    output reg [31:0] rdata
     );
     
+    reg [31:0] dmem[0:1023];
+    
     always@(*) begin
-        rdata
+        rdata = dmem[addr];
+    end
+    
+    always@(posedge clk or negedge resetn) begin
+        if (!resetn) begin 
+            dmem = {0};
+            wdata = 0;
+            rdata = 0;
+            
+        end
+        
+        else if (memwrite == 1)
+            dmem[addr] = wdata;
+            
+     end
 endmodule
