4cd17635730ed3bc2db3c7f60ecb9733e2a1f2e4
Nassersh1250 <nassersh1200@gmail.com>
2024-12-24 14:52:57 +0300

    Add files via upload

---
+++ Instruction_Memory.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/24/2024 01:32:10 PM
+// Design Name: 
+// Module Name: Instruction_Memory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Instruction_Memory(
+
+    );
+endmodule
