5c4d2bb0327368a1a71beb5b876bafd94d2dd034
Nassersh1250 <nassersh1200@gmail.com>
2024-12-24 15:58:38 +0300

    Add files via upload

---
+++ immediate_gen.sv
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+
+module immediate_gen(
+    input logic [31:0]inst,
+    output logic [31:0] imm
+    );
+    
+    always@(*) begin
+    case (inst[6:0]) 
+         7'b0010011: imm = { {20{inst[31]}}, inst[31:20] }; // I-type
+         7'b0000011: imm = { {20{inst[31]}}, inst[31:20] }; // I-type
+         7'b0100011: imm = { {20{inst[31]}}, inst[31:25], inst[11:7] }; // S-type
+         7'b1100011: imm = { {20{inst[31]}},  inst[31:25], inst[11:7]}; // B-type
+         default: imm = { {20{inst[31]}}, inst[31:20] }; // Default case for unsupported instructions
+        endcase
+        
+    end
+endmodule
